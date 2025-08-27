12410595f04abce924f5630afdb3c84b58340815
Avinash kumar <akum97543@gmail.com>
2025-08-27 21:42:18 +0530

    Creating a InventoryManagementSystem application basic folder struct

---
+++ src/test/java/com/avinash/InventoryManagementSystem/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.InventoryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
